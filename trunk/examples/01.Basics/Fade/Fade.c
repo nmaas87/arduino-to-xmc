6b514b8e935a96ba11177949bc00111d7d3a18cc
massimo.atti@gmail.com <massimo.atti@gmail.com@bdf4a455-bac5-7b25-77c5-9c60ffb28105>
2014-02-16 22:21:05 +0000

    [ADD]: Blink w/o Delay / Fade / HelloWord / Joystick examples code

---
+++ trunk/examples/01.Basics/Fade/Fade.c
@@ -0,0 +1,85 @@
+/*
+ * Main.c
+ *
+ */
+
+#define ARDUINO
+#define ARDUINO_MAIN
+#include "Arduino.h"
+
+#include <XMC1100.h>		//SFR declarations of the selected device
+#include <DAVE3.h>
+
+// #include Arduino HEADER FILES.
+#include "pins_arduino.h"
+#include "wiring_digital.h"
+#include "wiring_time.h"
+#include "wiring_clock.h"
+
+int main(void)
+{
+
+	/*
+	  *  Wiring Initialization
+	  */
+	wiring_digital_init();
+	wiring_analog_init();
+
+	/*
+	  *  Periferals Initialization
+	  */
+	DAVE_Init();
+
+	// Arduino's main() function just calls setup() and loop()....
+	setup();
+	while (1) {
+		loop();
+		//yield();
+	}
+}
+
+//****************************************************************************
+// 							       ARDUINO SKETCH
+//****************************************************************************
+
+/*
+ Fade
+ 
+ This example shows how to fade an LED on pin 13
+ using the analogWrite() function.
+ 
+ This example code is in the public domain.
+ */
+
+int led = 13;          // the pin that the LED is attached to
+int brightness = 0;    // how bright the LED is
+int fadeAmount = 5;    // how many points to fade the LED by
+
+// the setup routine runs once when you press reset:
+void setup()  { 
+  // declare LED to be an PWM output:
+  pinMode(led, PWM);
+
+  // Turn-Off LED1 sis is not used
+  pinMode(LED2, OUTPUT);
+} 
+
+// the loop routine runs over and over again forever:
+void loop()  { 
+  // set the brightness of LED:
+  analogWrite(led, brightness);    
+
+  // change the brightness for next time through the loop:
+  brightness = brightness + fadeAmount;
+
+  // reverse the direction of the fading at the ends of the fade: 
+  if (brightness == 0 || brightness == 255) {
+    fadeAmount = -fadeAmount ; 
+  }     
+  // wait for 30 milliseconds to see the dimming effect    
+  delay(30);                            
+}
+
+//****************************************************************************
+// 							       END OF FILE
+//****************************************************************************

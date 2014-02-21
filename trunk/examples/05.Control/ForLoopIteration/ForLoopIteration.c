88de2e2366a9683c725dc0ebf3b4da19ea629fc5
massimo.atti@gmail.com <massimo.atti@gmail.com@bdf4a455-bac5-7b25-77c5-9c60ffb28105>
2014-02-21 23:11:06 +0000

    [ADD]: ForLoop and Array examples on extended LEDs

---
+++ trunk/examples/05.Control/ForLoopIteration/ForLoopIteration.c
@@ -0,0 +1,95 @@
+/*
+ * Main.c
+ *
+ */
+
+#define ARDUINO
+#define ARDUINO_MAIN
+#include "Arduino.h"
+
+#include <XMC1100.h>		//SFR declarations of the selected device
+#include <DAVE3.h>
+
+// #include Arduino HEADER FILES.
+#include "pins_arduino.h"
+#include "wiring_digital.h"
+#include "wiring_time.h"
+#include "wiring_clock.h"
+
+int main(void)
+{
+
+	/*
+	  *  Wiring Initialization
+	  */
+	wiring_digital_init();
+	wiring_analog_init();
+
+	/*
+	  *  Periferals Initialization
+	  */
+	DAVE_Init();
+
+	// Arduino's main() function just calls setup() and loop()....
+	setup();
+	while (1) {
+		loop();
+		//yield();
+	}
+}
+
+//****************************************************************************
+// 							       ARDUINO SKETCH
+//****************************************************************************
+
+/*
+  For Loop Iteration
+ 
+ Demonstrates the use of a for() loop. 
+ Lights multiple LEDs in sequence, then in reverse.
+ 
+ The circuit:
+ * LEDs from pins 30 through 35 to ground
+ 
+ created 2006
+ by David A. Mellis
+ modified 30 Aug 2011
+ by Tom Igoe 
+
+This example code is in the public domain.
+ 
+ http://www.arduino.cc/en/Tutorial/ForLoop
+ */
+
+int timer = 100;           // The higher the number, the slower the timing.
+
+void setup() {
+  // use a for loop to initialize each pin as an output:
+  for (int thisPin = 30; thisPin < 36; thisPin++)  {
+    pinMode(thisPin, OUTPUT);      
+  }
+}
+
+void loop() {
+  // loop from the lowest pin to the highest:
+  for (int thisPin = 30; thisPin < 36; thisPin++) { 
+    // turn the pin on:
+    digitalWrite(thisPin, HIGH);   
+    delay(timer);                  
+    // turn the pin off:
+    digitalWrite(thisPin, LOW);    
+  }
+
+  // loop from the highest pin to the lowest:
+  for (int thisPin = 35; thisPin >= 30; thisPin--) { 
+    // turn the pin on:
+    digitalWrite(thisPin, HIGH);
+    delay(timer);
+    // turn the pin off:
+    digitalWrite(thisPin, LOW);
+  }
+}
+
+//****************************************************************************
+// 							       END OF FILE
+//****************************************************************************

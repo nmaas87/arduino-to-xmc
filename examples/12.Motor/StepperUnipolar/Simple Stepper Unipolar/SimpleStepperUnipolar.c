19273f00a42087d79fda7434a95b5c854dc85c8d
massimo.atti@gmail.com <massimo.atti@gmail.com@bdf4a455-bac5-7b25-77c5-9c60ffb28105>
2014-04-21 23:13:01 +0000

    [CHANGE]: move Stepper Motor from StarterKit into Motor example section

---
+++ examples/12.Motor/StepperUnipolar/Simple Stepper Unipolar/SimpleStepperUnipolar.c
@@ -0,0 +1,93 @@
+/*
+ * Main.c
+ *
+ */
+
+#define ARDUINO
+#define ARDUINO_MAIN
+#include "Arduino.h"
+
+#include <XMC1100.h>		//SFR declarations of the selected device
+#include <DAVE3.h>
+
+// #include Arduino HEADER FILES.
+#include "pins_arduino.h"
+#include "wiring_digital.h"
+#include "wiring_time.h"
+#include "wiring_clock.h"
+
+int main(void)
+{
+
+	/*
+	  *  Wiring Initialization
+	  */
+	wiring_digital_init();
+	wiring_analog_init();
+
+	/*
+	  *  Periferals Initialization
+	  */
+	DAVE_Init();
+
+	// Arduino's main() function just calls setup() and loop()....
+	setup();
+	while (1) {
+		loop();
+		//yield();
+	}
+}
+
+//****************************************************************************
+// 							       ARDUINO SKETCH
+//****************************************************************************
+
+/* Stepper Unipolar
+ *
+ * Example 1: Simple example
+ * -------------
+ *
+ * Program to drive a stepper motor coming ETT
+ * [ftp://ulita.ms.mff.cuni.cz/predn/NPRG037/ALL_MINI_IO.pdf]
+ *
+ */
+
+int motorPin1 = 8;
+int motorPin2 = 9;
+int motorPin3 = 10;
+int motorPin4 = 11;
+int delayTime = 10;
+
+void setup() {
+  pinMode(motorPin1, OUTPUT);
+  pinMode(motorPin2, OUTPUT);
+  pinMode(motorPin3, OUTPUT);
+  pinMode(motorPin4, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(motorPin1, HIGH);
+  digitalWrite(motorPin2, LOW);
+  digitalWrite(motorPin3, LOW);
+  digitalWrite(motorPin4, LOW);
+  delay(delayTime);
+  digitalWrite(motorPin1, LOW);
+  digitalWrite(motorPin2, HIGH);
+  digitalWrite(motorPin3, LOW);
+  digitalWrite(motorPin4, LOW);
+  delay(delayTime);
+  digitalWrite(motorPin1, LOW);
+  digitalWrite(motorPin2, LOW);
+  digitalWrite(motorPin3, HIGH);
+  digitalWrite(motorPin4, LOW);
+  delay(delayTime);
+  digitalWrite(motorPin1, LOW);
+  digitalWrite(motorPin2, LOW);
+  digitalWrite(motorPin3, LOW);
+  digitalWrite(motorPin4, HIGH);
+  delay(delayTime);
+}
+
+//****************************************************************************
+// 							       END OF FILE
+//****************************************************************************
